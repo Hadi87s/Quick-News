a374c41efd683d1000c1ecc346b0309111ef2718
Hadi87s <hadiirshaid8722@gmail.com>
2025-02-22 23:46:48 +0200

    adding a component from reactBits, its the best thing I've ever seen!

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import HeroGeometric from "@/components/hero-geometrics/HeroGeometrics";
+import ScrollVelocity from "@/components/scroll-velocity/ScrollVelocity";
 
 export default function Home() {
+  const velocity = 41;
   return (
     <>
       <HeroGeometric />
+
+      <ScrollVelocity
+      texts={['Hadi is trying reactBits for the first time', 'And reactBits is COOL!']} 
+      velocity={velocity} 
+      className="custom-scroll-text p-2"
+    />
     </>
   );
 }
